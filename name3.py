cb126f95a46a57e0c25f2f4b2a18a4cee4644fc1
Lakshanya14 <newhotandspicy14@gmail.com>
2025-05-17 16:41:10 +0530

    Add files via upload

---
+++ name3.py
@@ -0,0 +1,21 @@
+print("🎥 hello my friend!")
+name=input("what is your name?")
+print(f"Hello {name} I can help you find some new movies to watch!")
+print("\nWhat kind of movies do you like")
+print("1. action")
+print("2. comedy")
+print("3. animated")
+print("4. horror")
+choice=input("please enter the number of your choice:")
+if choice=="1":
+    print("I recommend you watch 'CHHAAVA🫡','HANUMAN','K.G.F.'")
+elif choice=="2":
+    print("I recommend you to watch 'GOLMAAL🤣','ENTERTAINMENT😂'")
+elif choice=="3":
+    print("I recommend you to watch 'TOM AND JERRY🐱‍👤','POKEMON🐉'")
+elif choice=="4":
+    print("I recommend you to watch 'RAAZ👻','BHOOT👺'")
+else:
+    print("I am sorry,I cannot help you with that")
+
+print("\nI hope you liked watching these movies.😊")
